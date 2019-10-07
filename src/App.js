0183a996b81d4337b22f163ed6e99dcb4c32b5a2
Tony Lomax <antl.lomax@gmail.com>
2019-10-07 16:55:18 +0100

    Lambda functions deploy to netlify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./functions/APICall";
 
-function App() {
+// const fetchData = async () => {
+//   await (await fetch("http://localhost:9000/APICall")).json();
+// };
+
+function App(props) {
+  const [data, setData] = React.useState([]);
+  const [names, setNames] = React.useState([]);
+  const [skills, setSkills] = React.useState([]);
+  const [activity, setActivity] = React.useState("");
+  const [date, setDate] = React.useState("");
+  const [duration, setDuration] = React.useState("");
+  const [link, setLink] = React.useState("");
+  React.useEffect(() => {
+    fetch("/.netlify/functions/APICall")
+      .then(res => res.json())
+      .then(res => {
+        setData(res.records);
+        setNames(res.records.map(e => e.fields.name));
+        setSkills(res.records.map(e => e.fields.skills));
+        setActivity(res.records.map(e => e.fields.nameOfActivity));
+      });
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>
+        {names} {skills} {activity}
+      </h1>
+      {/* <p> {data[1].fields} </p> */}
+
+      {/* {data.map(e => (
+          <li key={e.id}>hi</li>
+
+          // <Repo key={repo.id} {...repo} />
+        ))} */}
     </div>
   );
 }
