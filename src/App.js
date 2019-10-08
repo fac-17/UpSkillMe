d04029ac65908ed75210f0f83e91a75311343862
andy <andy.william.mcdonald@gmail.com>
2019-10-08 10:55:36 +0100

    added profile component relates #20

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React from "react";
 import "./App.css";
-import Badges from "./components/badges/Badges"
+import Badges from "./components/badges/Badges";
+import Profile from "./components/profile/Profile";
 
 
 function App(props) {
   const [data, setData] = React.useState([]);
-  const [names, setNames] = React.useState([]);
+  const [name, setName] = React.useState(["Joseph McBloggs"]);
   const [skills, setSkills] = React.useState([]);
   const [activity, setActivity] = React.useState("");
   const [date, setDate] = React.useState("");
   const [duration, setDuration] = React.useState("");
   const [link, setLink] = React.useState("");
+  const [avatar, setAvatar] = React.useState("assets/avatarAlien.svg");
+  const [totalScore, setTotalScore] = React.useState(10);
   React.useEffect(() => {
     fetch("/.netlify/functions/APICall")
       .then(res => res.json())
       .then(res => {
         setData(res.records);
-        setNames(res.records.map(e => e.fields.name));
+        setName(res.records.map(e => e.fields.name));
         setSkills(res.records.map(e => e.fields.skills));
         setActivity(res.records.map(e => e.fields.nameOfActivity));
       });
   }, []);
 
-
   return (
     <div className="App">
-      <h1>
-        {names} {skills} {activity}
-      </h1>
+      < Profile name={name} avatar={avatar} totalScore={totalScore} />
+
+
       <Badges selectedBadges={[
         "Innovation",
         "Teamwork",
