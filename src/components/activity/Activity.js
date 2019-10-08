1cf6188bdabd5b281f765046ed875aa731616d52
andy <andy.william.mcdonald@gmail.com>
2019-10-08 11:41:08 +0100

    activity badges rendering relates #20

--- src/components/activity/Activity.js
+++ src/components/activity/Activity.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Badges from '../../components/badges/Badges';
 
 export default function Activity({ activity }) {
     return (<ul>
         <li>{activity.name}</li>
         <li>{activity.date}</li>
         <li>{activity.duration}</li>
-        <li>{activity.badges}</li>
+        <li><Badges selectedBadges={activity.badges} />
+        </li>
         <li>{activity.activityScore}</li>
         <li>{activity.link}</li>
     </ul>)
