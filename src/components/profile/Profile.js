a2bcbe0b1e216b36b94b1b0eb1ca7abc52f5661a
Emmanuel <emanagyei@gmail.com>
2019-10-09 16:55:17 +0100

    add function to display total points Relates #19

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,6 +1,19 @@
 import React from "react";
 
-export default function Profile({ name, avatar, totalScore }){
+
+export default function Profile({ name, avatar, data }){
+    const [totalScore, setTotalScore] = React.useState(10);
+    React.useEffect(()=>{
+        if (data) {
+    let tempTotalPoints = 0
+    data.forEach(activity=>{
+        console.log(activity.fields)
+        tempTotalPoints+=activity.fields['totalPoints (Activity points x duration)']
+    }) 
+    setTotalScore(tempTotalPoints)
+}
+
+    },[data])
     return(
         <div>
         <h1>{name}</h1>
