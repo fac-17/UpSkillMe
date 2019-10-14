aeae8edd1aba852f9eb0f95cc443b2448a877a8d
andy <andy.william.mcdonald@gmail.com>
2019-10-14 15:42:31 +0100

    some landing page style

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,31 @@
 import React from "react";
 import LogInForm from "../components/log-in-form/LogInForm";
 import SignUpForm from "../components/sign-up-form/SignUpForm"
+import styled from "styled-components";
+
+const Div = styled.div` 
+  display: block;
+  width: 90%;
+  margin: 20px;
+  // background-color: #ecebe4;
+`;
+
+const H1 = styled.h1`
+  text-align: center;
+  margin-bottom: 40px;
+`;
+
+// export default function Profile({ data, emailInput }) {
+//   const [avatar, setAvatar] = React.useState("assets/avatarAlien.svg");
+
 
 export default function LogInPage({ emailInput, setEmailInput }) {
+  const [logo, setLogo] = React.useState("assets/UpSkillMe_logo.png");
   return (
-    <div>
-      <h1>Welcome to UpSkillMe</h1>
+    <Div>
+      <H1><img src={logo} alt="logo" width="150px" /></H1>
       <LogInForm emailInput={emailInput} setEmailInput={setEmailInput} />
       <SignUpForm emailInput={emailInput} setEmailInput={setEmailInput} />
-    </div>
+    </Div>
   );
 }
