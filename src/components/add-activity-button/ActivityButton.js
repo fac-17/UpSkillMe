317aa950af37d48342f13407db04d78fd974f2fb
andy <andy.william.mcdonald@gmail.com>
2019-10-14 12:15:36 +0100

    button, activities and profile styled components

--- src/components/add-activity-button/ActivityButton.js
+++ src/components/add-activity-button/ActivityButton.js
@@ -1,5 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+
+//Styled components
+const ActivityButtonStyle= styled.button`
+  border-radius: 100%;
+  width: 100px;
+  height: 100px;
+  position: fixed;
+  bottom: 0px;
+  right: 0px;
+  font-size: 1em;
+  background-color: #109cf1;
+  color: white;
+`;
+
+const PlusStyle = styled.span`
+  font-size: 1.5em;
+  color: white;
+`;
+
+//
 
 export default function ActivityButton() {
-  return <button>Add Activity</button>;
+  return <ActivityButtonStyle>
+    Add Activity <br></br>
+    <PlusStyle>+</PlusStyle>
+    </ActivityButtonStyle>;
 }
