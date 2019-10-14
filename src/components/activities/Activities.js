2a49050261d4a981ad7a82c6a3cb524c740c70ff
Tony Lomax <antl.lomax@gmail.com>
2019-10-14 14:26:24 +0100

    Fixed activites map key issue

--- src/components/activities/Activities.js
+++ src/components/activities/Activities.js
@@ -5,6 +5,7 @@ export default function Activities({ activities }) {
   if (!activities) {
     return <h3> Activities not loaded...</h3>;
   }
+  console.log("these are the activiteis", activities);
   return (
     <div>
       <h2>Activity log</h2>
@@ -12,7 +13,14 @@ export default function Activities({ activities }) {
         Displaying {activities.length} of {activities.length}
       </h3>
       {activities.map((activity, index) => {
-        return <Activity activity={activity} index={index} activities={activities} />;
+        return (
+          <Activity
+            key={activity.id}
+            activity={activity}
+            index={index}
+            activities={activities}
+          />
+        );
       })}
     </div>
   );
