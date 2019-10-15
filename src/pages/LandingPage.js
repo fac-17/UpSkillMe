b9ff33409398af7ed6d3b4d2d2b07d95b27ef4b4
Tony Lomax <antl.lomax@gmail.com>
2019-10-15 17:23:56 +0100

    Resolving merge

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,58 +1,158 @@
 import React from "react";
 import LogInForm from "../components/log-in-form/LogInForm";
-import SignUpForm from "../components/sign-up-form/SignUpForm"
+import SignUpForm from "../components/sign-up-form/SignUpForm";
 import { ThemeProvider } from "styled-components";
-import theme from '../theme';
-import { ImageDescription, Div, Image, CircleSelection, Circle, SignUpButton, ButtonContainer, InfoSection, FormSection } from "./LandingPageStyle";
-import { Button } from '../components/common/common'
-import { LANDING_PAGE_TEXT_DESCRIPTIONS as TEXTDESCRIPTION } from '../constants';
-import setSliderInterval from '../utils/setSliderInterval';
+import theme from "../theme";
+import {
+  ImageDescription,
+  Div,
+  Image,
+  CircleSelection,
+  Circle,
+  SignUpButton,
+  ButtonContainer,
+  InfoSection,
+  FormSection
+} from "./LandingPageStyle";
+import { Button } from "../components/common/common";
+import { LANDING_PAGE_TEXT_DESCRIPTIONS as TEXTDESCRIPTION } from "../constants";
+import setSliderInterval from "../utils/setSliderInterval";
 
 export default function LogInPage({ emailInput, setEmailInput }) {
   const [logo, setLogo] = React.useState("assets/UpSkillMe_logo.png");
-  const [infoSlider, setInfoSlider] = React.useState('achievements')
-  const [pageState, setPageState] = React.useState('');
+  const [infoSlider, setInfoSlider] = React.useState("achievements");
+  const [pageState, setPageState] = React.useState("");
 
   let intervalId;
   React.useEffect(() => {
-    intervalId = setSliderInterval(infoSlider, setInfoSlider, 3000)
-    return () => { clearInterval(intervalId) }
-  }, [infoSlider])
+    intervalId = setSliderInterval(infoSlider, setInfoSlider, 3000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [infoSlider]);
 
   return (
     <ThemeProvider theme={theme}>
       <Div>
         <Image src={logo} alt="logo" width="100px" />
-        <InfoSection pageState={pageState} >
-          <img src={`assets/${infoSlider}.svg`} alt="info image" width="150px" />
+        <InfoSection pageState={pageState}>
+          <img
+            src={`assets/${infoSlider}.svg`}
+            alt="info image"
+            width="150px"
+          />
           <ImageDescription>{TEXTDESCRIPTION[infoSlider]}</ImageDescription>
           <CircleSelection>
-
-            <Circle onClick={() => { setInfoSlider('achievements') }} width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="15" cy="15" r="15" fill={infoSlider === 'achievements' ? theme.primaryColor : theme.secondaryGreyColor} />
+            <Circle
+              onClick={() => {
+                setInfoSlider("achievements");
+              }}
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                cx="15"
+                cy="15"
+                r="15"
+                fill={
+                  infoSlider === "achievements"
+                    ? theme.primaryColor
+                    : theme.secondaryGreyColor
+                }
+              />
             </Circle>
-            <Circle onClick={() => { setInfoSlider('opportunities') }} width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="15" cy="15" r="15" fill={infoSlider === 'opportunities' ? theme.primaryColor : theme.secondaryGreyColor} />
+            <Circle
+              onClick={() => {
+                setInfoSlider("opportunities");
+              }}
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                cx="15"
+                cy="15"
+                r="15"
+                fill={
+                  infoSlider === "opportunities"
+                    ? theme.primaryColor
+                    : theme.secondaryGreyColor
+                }
+              />
             </Circle>
-            <Circle onClick={() => { setInfoSlider('potential') }} width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="15" cy="15" r="15" fill={infoSlider === 'potential' ? theme.primaryColor : theme.secondaryGreyColor} />
+            <Circle
+              onClick={() => {
+                setInfoSlider("potential");
+              }}
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                cx="15"
+                cy="15"
+                r="15"
+                fill={
+                  infoSlider === "potential"
+                    ? theme.primaryColor
+                    : theme.secondaryGreyColor
+                }
+              />
             </Circle>
           </CircleSelection>
           <ButtonContainer>
-            <Button className="show-login" onClick={() => { setPageState('login') }}>Login</Button>
-            <Button className="show-signup" onClick={() => { setPageState('signup') }}>Signup</Button>
+            <Button
+              className="show-login"
+              onClick={() => {
+                setPageState("login");
+              }}
+            >
+              Login
+            </Button>
+            <Button
+              className="show-signup"
+              onClick={() => {
+                setPageState("signup");
+              }}
+            >
+              Signup
+            </Button>
           </ButtonContainer>
-        </InfoSection >
+        </InfoSection>
 
-        {pageState === 'login' ?
+        {pageState === "login" ? (
           <FormSection>
             <LogInForm emailInput={emailInput} setEmailInput={setEmailInput} />
-            <Button onClick={() => { setPageState('') }}>Back</Button>
-          </FormSection> : pageState === 'signup' ?
-            <FormSection><SignUpForm emailInput={emailInput} setEmailInput={setEmailInput} />
-              <Button onClick={() => { setPageState('') }}>Back</Button>
-            </FormSection> : ''}
-      </Div >
-    </ThemeProvider >
+            <Button
+              onClick={() => {
+                setPageState("");
+              }}
+            >
+              Back
+            </Button>
+          </FormSection>
+        ) : pageState === "signup" ? (
+          <FormSection>
+            <SignUpForm emailInput={emailInput} setEmailInput={setEmailInput} />
+            <Button
+              onClick={() => {
+                setPageState("");
+              }}
+            >
+              Back
+            </Button>
+          </FormSection>
+        ) : (
+          ""
+        )}
+      </Div>
+    </ThemeProvider>
   );
 }
