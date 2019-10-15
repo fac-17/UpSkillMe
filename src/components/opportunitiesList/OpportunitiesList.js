a5cf731a0c07702786226fcce5aa40510de9da11
Emmanuel <emanagyei@gmail.com>
2019-10-15 12:11:18 +0100

    add opportunity.js file

--- src/components/opportunitiesList/OpportunitiesList.js
+++ src/components/opportunitiesList/OpportunitiesList.js
@@ -1,2 +1,26 @@
 import React from "react";
+// import Opportunity from "";
 
+export default function OpportunitiesList({ opportunities }) {
+  if (!opportunities) {
+    return <h3>Opportunities not loaded...</h3>;
+  }
+  return (
+    <div>
+      <h2>Opportunities List</h2>
+      <h3>
+        Displaying {opportunities.length} of {opportunities}
+      </h3>
+      {opportunities.map((opportunity, index) => {
+        return (
+          <Opportunity
+            key={opportunity.id}
+            opportunity={opportunity}
+            //   index={index}
+            //   opportunities={opportunities}
+          />
+        );
+      })}
+    </div>
+  );
+}
