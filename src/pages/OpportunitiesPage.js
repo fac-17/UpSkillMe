c00a7b9264878ca1907b88e5956761267236d399
Emmanuel <emanagyei@gmail.com>
2019-10-15 14:30:12 +0100

    add Opportunities page Relates #64

--- src/pages/OpportunitiesPage.js
+++ src/pages/OpportunitiesPage.js
@@ -1,8 +1,23 @@
 import React from "react";
+import OpportunitiesList from "../components/opportunitiesList";
 import { Redirect, Route } from "react-router-dom";
+import LogOutButton from "../components/log-out-button/log-out-button";
 
-export default function OpportunitiesPage({
-    // setOpportunites
+export default function OpportunitiesPage({ opportunities }) {
+  const [loggedOut, setLoggedOut] = React.useState(false);
 
+  if (loggedOut) {
+    return (
+      <Route>
+        <Redirect to={{ pathname: "/" }} />
+      </Route>
+    );
+  }
+
+  return (
+    <div>
+      <LogOutButton setLoggedOut={setLoggedOut} />
+      <OpportunitiesList>{opportunities}</OpportunitiesList>
+    </div>
+  );
 }
-)
