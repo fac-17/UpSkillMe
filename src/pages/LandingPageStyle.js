80144680b82aa38ea8ec38bbc5e22307cd6318a3
Tony Lomax <antl.lomax@gmail.com>
2019-10-15 15:47:03 +0100

    Theme set up

--- src/pages/LandingPageStyle.js
+++ src/pages/LandingPageStyle.js
@@ -1,61 +1,69 @@
 import styled from "styled-components";
 
-const Div = styled.div` 
+const Div = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  color: ${(props) => props.theme.primaryColor};
+  color: ${props => props.theme.primaryColor};
 `;
 
 const Image = styled.img`
-
   margin: 2rem 0;
-
 `;
 
 const CircleSelection = styled.div`
-/* display: ${pageState => pageState === 'start' ? 'block' : 'hidden'}; */
+/* display: ${pageState => (pageState === "start" ? "block" : "hidden")}; */
 display: flex;
 flex-direction: row;
 align-items: center;
-`
+`;
 const Circle = styled.svg`
   margin: 0 0.5rem;
-`
+`;
 const Button = styled.button`
   border-radius: 0.5rem;
-width: 6rem;
-height: 2rem;
-font-size: 1rem;
-text-align: center;
-margin: 3rem  0.5rem;
-`
+  width: 6rem;
+  height: 2rem;
+  font-size: 1rem;
+  text-align: center;
+  margin: 3rem 0.5rem;
+`;
 const SignUpButton = styled.button`
-border-radius: 0.5rem;
-width: 6rem;
-height: 2rem;
-font-size: 1rem;
-text-align: center;
-margin: 3rem  0.5rem;
-margin-left:auto;
-margin-right:auto;
-`
+  border-radius: 0.5rem;
+  width: 6rem;
+  height: 2rem;
+  font-size: 1rem;
+  text-align: center;
+  margin: 3rem 0.5rem;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 const ButtonContainer = styled.div`
   display: flex;
   flex-direction: row;
-`
+`;
 
 const InfoSection = styled.div`
-display: ${props => props.pageState === '' ? 'flex' : 'none'};
-flex-direction: column;
-align-items: center;
-`
+  display: ${props => (props.pageState === "" ? "flex" : "none")};
+  flex-direction: column;
+  align-items: center;
+`;
 
 const FormSection = styled.section`
-display:flex;
-width:100%;
-flex-direction: column;
-align-items: center;
-`
-export { Div, Image, CircleSelection, Button, Circle, SignUpButton, ButtonContainer, InfoSection, FormSection }
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+`;
+export {
+  Div,
+  Image,
+  CircleSelection,
+  Button,
+  Circle,
+  SignUpButton,
+  ButtonContainer,
+  InfoSection,
+  FormSection
+};
