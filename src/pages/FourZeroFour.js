58276b59ce75e8ef8b2d9c243da1fa734a063936
Tony Lomax <antl.lomax@gmail.com>
2019-10-16 12:46:00 +0100

    Button to return home aded to 404 page

--- src/pages/FourZeroFour.js
+++ src/pages/FourZeroFour.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Redirect, Route } from "react-router-dom";
 
 import { ThemeProvider } from "styled-components";
 import theme from "../theme";
 
 export default function FourZeroFour() {
-  return <h1>Sorry, that page doesn't exist!</h1>;
+  return (
+    <section>
+      <h1>Sorry, that page doesn't exist!</h1>
+      <a href="/">
+        <button>Click to return to log on page</button>
+      </a>
+    </section>
+  );
 }
