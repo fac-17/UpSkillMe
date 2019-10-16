9d13ef475e3124cd192886564e5930b2ea76ca57
Tony Lomax <antl.lomax@gmail.com>
2019-10-16 12:36:15 +0100

    Basic 404 page rendering

---
+++ src/pages/FourZeroFour.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import { ThemeProvider } from "styled-components";
+import theme from "../theme";
+
+export default function FourZeroFour() {
+  return <h1>Sorry, that page doesn't exist!</h1>;
+}
