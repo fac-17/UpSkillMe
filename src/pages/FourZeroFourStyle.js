a32f98deed3d11ebf11b9751bffec62d8016913b
Emmanuel <emanagyei@gmail.com>
2019-10-17 15:33:03 +0100

    fix issue with 404 margin Relates #74

--- src/pages/FourZeroFourStyle.js
+++ src/pages/FourZeroFourStyle.js
@@ -15,6 +15,7 @@ const FourZeroFourSection = styled.section`
 const H1 = styled.h1`
   font-size: 3em;
   padding-top: 25px;
+  margin: 0px 0px 1rem 0px;
   color: ${props => props.theme.mainWhiteColor};
 `;
 
@@ -28,7 +29,7 @@ const ReturnButton = styled.button`
   height: 2rem;
   font-size: 0.8rem;
   text-align: center;
-  margin: 3rem 0.5rem;
+  margin: 0;
   margin-left: auto;
   margin-right: auto;
   background: ${props => props.theme.mainWhiteColor};
