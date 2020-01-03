e13dc68d23f8b0ea744a4c697060bffd2b686fd8
evgeniidatsiuk <evgenii.datsiuk@gmail.com>
2020-01-04 00:20:33 +0200

    Add media && fix paddings in FilterButton

--- src/components/filter-opportunity-button/FilterOpportunityButton.style.js
+++ src/components/filter-opportunity-button/FilterOpportunityButton.style.js
@@ -5,8 +5,7 @@ const ButtonDiv = styled.div`
   width: 90%;
   max-width: 750px;
   margin: 1.5rem auto;
-  padding-top: 2rem;
-  padding-bottom: 2rem;
+  padding-bottom: 1.5rem;
   list-style: none;
   align-items: center;
   text-align: center;
@@ -25,13 +24,18 @@ const FilterOpportunityButtonStyle = styled.button`
   margin-left: 15px;
   margin-bottom: 15px;
   background-color: inherit;
-  border: 1px solid black;
-  border-radius: 10px;
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  border-radius: 18px;
   :focus {
     background: #1592DE;
     h3${H3} {
       color: white;
     }
+  }
+  @media (max-width: 615px) {
+    margin-left: 8px;
+    margin-bottom: 12px;
   }
 `;
 
